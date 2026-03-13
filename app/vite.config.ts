ec91a2b607ebd6136966e18850f4735494699a30
Kurre Chaitanya <kurre.woc@gmail.com>
2026-03-13 20:28:37 +0530

    fix: disable css minify

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,13 +2,16 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    cssMinify: false,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
+```
